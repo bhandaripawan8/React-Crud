21ad731d14301fab0b25cb3cc22a36cff094beb9
Pawan <pawanb78@gmail.com>
2024-01-21 01:18:45 +0000

    read and upadate screen done

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom';
+axios
 
 export default function Read() {
+  const [data,setdata] = useState({});
+  // useparam hook will get the id from the url
+  const{id} = useParams();
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/users/'+id)
+    .then(res=> setdata(res.data))
+    .catch(err=> console.error(err))
+  },[])
   return (
-    <div>Read</div>
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
+      <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded '>
+          <h3>Details of the User</h3>
+          <div className='mb-2'>
+            <strong>Name: {data.name}</strong>
+          </div>
+          <div className='mb-2'>
+            <strong>Email: {data.email}</strong>
+          </div>
+          <div className='mb-2'>
+            <strong>Phone Number: {data.phone}</strong>
+          </div>
+          <Link to={`/update/${id}`} className='btn btn-success'>Edit</Link>
+          <Link to={`/`} className='btn btn-primary ms-3'>Back</Link>
+      </div>
+    </div>
   )
 }
+
--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import {React, useState, useEffect} from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+
+
 
 export default function Update() {
+  // const [data, setdata] = useState([]);
+  const {id} = useParams();
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/users/'+id)
+    .then(res => setvalues(res.data))
+    .catch(err=> console.error(err));
+  },[])
+
+  const [values, setvalues] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  });
+
+  const navigate = useNavigate();
+  const handleUpdate = (event) =>{
+    event.preventDefault();
+    axios.put('http://localhost:3000/users/'+id, values)
+    .then(res=>{
+      console.log(res);
+      navigate('/');
+    })
+  }
+
   return (
-    <div>Update</div>
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
+    <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+      <h1>Update a User</h1>
+      <form onSubmit={handleUpdate}>
+        <div className="mb-2">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name='name' className='form-control' placeholder='Enter name...' value={values.name} onChange={(e) => setvalues({...values, name: e.target.value})}/>
+        </div>
+        <div className="mb-2">
+          <label htmlFor="email">Email:</label>
+          <input type="text" name='name' className='form-control' placeholder='Enter email address...'  value = {values.email} onChange = {e => setvalues({...values,email: e.target.value })}/>
+        </div>
+        <div className="mb-2">
+          <label htmlFor="phone">Phone:</label>
+          <input type="text" name='name' className='form-control' placeholder='Enter phone number...'  value = {values.phone} onChange = {e =>setvalues({...values, phone:e.target.value})}/>
+        </div>
+        <button className='btn btn-success'>Submit</button>
+        <Link to={"/"} className='btn btn-primary ms-3'>back</Link>
+      </form>
+    </div>
+  </div>
   )
 }
